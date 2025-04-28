3c7c9c41f2d4b39ea504270c63dc0c45944ed6ee
yjgaia <yjdev88@gmail.com>
2025-04-28 17:20:37 +0900

    Add initial implementation of ImageCombiner with blending functionality and configuration files

---
+++ lib/ImageCombiner.d.ts
@@ -0,0 +1,8 @@
+declare class ImageCombiner {
+    private blendPixel;
+    private blendImage;
+    combine(buffers: ArrayBuffer[]): ArrayBuffer;
+}
+declare const _default: ImageCombiner;
+export default _default;
+//# sourceMappingURL=ImageCombiner.d.ts.map
